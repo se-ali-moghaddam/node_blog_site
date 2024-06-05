39b41c7a917a106143b207ff7abf0cfd27afe518
Ali_Moghaddam <se.ali.moghaddam@gmail.com>
2024-06-05 07:27:15 -0700

    style added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 app.listen(3000);
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('index', {title: 'Home'});
