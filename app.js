0ca75246d287a7a5f33453b2b302037ae5dff856
Ali_Moghaddam <se.ali.moghaddam@gmail.com>
2024-06-09 03:29:43 -0700

    Added Express ejs Layouts

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
 app.listen(3000);
 
-app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(expressLayouts);
+
+app.set('layout', './layouts/default-layout')
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('index', {title: 'Home'});
 });
 
+app.get('/blog/:id', (req, res) => {
+    res.render('blog', {title: `Blog ${req.params.id}`});
+});
