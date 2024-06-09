732022633d29effd4705d86e78890334b0210c55
Ali_Moghaddam <se.ali.moghaddam@gmail.com>
2024-06-09 11:09:55 -0700

    Layout Completed

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ app.listen(3000);
 app.use(express.static('public'));
 app.use(expressLayouts);
 
-app.set('layout', './layouts/default-layout')
+app.set('layout', './layouts/default-layout');
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('index', {title: 'Home'});
+});
+
+app.get('/blogs/add', (req, res) => {
+    res.render('create-blog', {title: 'Create New Blog'});
 });
 
 app.get('/blog/:id', (req, res) => {
